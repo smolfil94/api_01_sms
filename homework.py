68622af47ea01d5c92c317b269beccbb32bfd7b4
Filipenkov Nikita <nikismol94@yandex.ru>
2020-12-23 22:34:06 +0300

    first commit

--- homework.py
+++ homework.py
@@ -1,20 +1,38 @@
-import time
+import os
 import requests
+import time
 
 from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv()
+
+URL = 'https://api.vk.com/method/users.get'
 
 
 def get_status(user_id):
+    access_token = os.getenv('VK_TOKEN')
     params = {
-        ...
+        'user_ids': user_id,
+        'fields': 'online',
+        'v': '5.92',
+        'access_token': access_token
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    status = requests.post(URL, params)
+    return status.json()['response'][0]['online']  # Верните статус пользователя в ВК
 
 
-def send_sms(sms_text, client):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+def sms_sender(sms_text):
+    account_sid = os.getenv('ACCOUNT_SID')
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+    number_from = os.getenv('NUMBER_FROM')
+    number_to = os.getenv('NUMBER_TO')
+    message = client.messages.create(
+        body=sms_text,
+        from_=number_from,
+        to=number_to
+    )
+    return message.sid  # Верните sid отправленного сообщения из Twilio
 
 
 if __name__ == '__main__':
@@ -22,6 +40,6 @@ if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
